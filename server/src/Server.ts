b1c75aedb395d1470d1ff129030d0fd24cd1a08b
roney ruiz <roneyruizrojas@gmail.com>
2025-09-06 17:47:53 -0600

    feat(server): create exampe of express api

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,1 +1,16 @@
-console.log("Starting server...");
+import http from "node:http"
+import express from "express"
+import { Bootstrap } from "./Bootstrap"
+import type { Request, Response } from "express"
+
+const app = express()
+const bootstrap = new Bootstrap()
+const server = http.createServer(app)
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello, World!" });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
