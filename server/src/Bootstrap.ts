d943e251782f22c7f16e3c64f450587fb45f06b6
roney ruiz <roneyruizrojas@gmail.com>
2025-09-06 22:28:12 -0600

    feat(server): update bootstrap component

--- server/src/Bootstrap.ts
+++ server/src/Bootstrap.ts
@@ -1,1 +1,27 @@
-export class Bootstrap {};
+import http from "node:http"
+import { json } from "express";
+
+import type { Express } from "express";
+import { PingRouter } from "@Routers/PingRouter";
+
+export class Bootstrap {
+  private server: http.Server;
+
+  constructor(private app: Express) {
+    this.server = http.createServer(app);
+  };
+
+  public addRouting(): void {
+    this.app.use("/ping", PingRouter);
+  };
+
+  public addMiddleware(): void {
+    this.app.use(json());
+  };
+
+  public startup(port: number): void {
+    this.server.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  };
+};
--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,16 +1,9 @@
-import http from "node:http"
 import express from "express"
 import { Bootstrap } from "./Bootstrap"
-import type { Request, Response } from "express"
 
 const app = express()
-const bootstrap = new Bootstrap()
-const server = http.createServer(app)
+const bootstrap = new Bootstrap(app)
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello, World!" });
-});
-
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+bootstrap.addRouting();
+bootstrap.addMiddleware();
+bootstrap.startup(3000);
