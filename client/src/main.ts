055e337f667011e430d22da8278c8f7cffb168f5
roney ruiz <roneyruizrojas@gmail.com>
2025-09-07 21:28:17 -0600

    chore(client): remove default code

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import "./style.css";
+import Main from "@App/Main.vue";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+
+const app = createApp(Main);
+
+app.mount("#app");
