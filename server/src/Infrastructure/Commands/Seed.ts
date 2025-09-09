4d54247bda6d124243faf3d3d03bd67233e34fc4
roney ruiz <roneyruizrojas@gmail.com>
2025-09-08 23:16:02 -0600

    feat(server): create seed command

---
+++ server/src/Infrastructure/Commands/Seed.ts
@@ -0,0 +1,29 @@
+import { Seeder } from "@Contracts/Seeder";
+import { Command } from "@Contracts/Command";
+import { AdminSeeder } from "@Seeders/AdminSeeder";
+
+import { eq } from "drizzle-orm";
+import { Seeders } from "@Models/Seeders";
+import { Drizzle } from "@Database/Drizzle";
+
+class Seed extends Command {
+  private seeders: Seeder[] = [
+    new AdminSeeder()
+  ];
+
+  public async run(): Promise<void> {
+    for (const seeder of this.seeders) {
+      const existing = await Drizzle.select()
+        .from(Seeders).where(eq(Seeders.name, seeder.name));
+
+      if (existing.length) continue;
+      await seeder.seed();
+
+      await Drizzle.insert(Seeders).values({ name: seeder.name });
+      console.log(`Seeded: ${seeder.name}`);
+    };
+  };
+};
+
+const command = new Seed();
+await command.run();
