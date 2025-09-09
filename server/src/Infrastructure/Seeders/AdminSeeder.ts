6935a6edad41cbdf556fffa32c858ed451d8c91f
roney ruiz <roneyruizrojas@gmail.com>
2025-09-08 23:15:52 -0600

    feat(server): create admin seeder

---
+++ server/src/Infrastructure/Seeders/AdminSeeder.ts
@@ -0,0 +1,16 @@
+import { Users } from "@Models/Users";
+import { Seeder } from "@Contracts/Seeder";
+import { Drizzle } from "@Database/Drizzle";
+
+export class AdminSeeder extends Seeder {
+  public name: string = "admin-seeder";
+
+  public async seed(): Promise<void> {
+    const admin: typeof Users.$inferInsert = {
+      name: process.env.ADMIN_NAME!,
+      password: process.env.ADMIN_PASSWORD!
+    };
+
+    await Drizzle.insert(Users).values(admin);
+  };
+};
