26d33fce4cfaa4deef747bc3894e529c9596696a
roney ruiz <roneyruizrojas@gmail.com>
2025-09-09 21:39:15 -0600

    chore(server): remove old contracts

--- server/src/Domain/Contracts/Command.ts
+++
@@ -1,3 +0,0 @@
-export abstract class Command {
-  public abstract run(): Promise<void>;
-};
--- server/src/Domain/Contracts/Seeder.ts
+++
@@ -1,4 +0,0 @@
-export abstract class Seeder {
-  public name: string = "<seeder-name>";
-  public abstract seed(): Promise<void>;
-};
