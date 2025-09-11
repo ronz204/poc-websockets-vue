0d65cf40ee8c03f0566b1b284661d6148f63a258
roney ruiz <roneyruizrojas@gmail.com>
2025-09-11 16:25:25 -0600

    chore(server): rename models

--- server/src/Domain/Models/Seeder.ts
+++ server/src/Domain/Models/Seeder.ts
@@ -1,7 +1,6 @@
 import { pgTable, integer, varchar } from "drizzle-orm/pg-core";
 
-export const Users = pgTable("users", {
+export const Seeder = pgTable("seeders", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  password: varchar({ length: 255 }).notNull(),
 });
--- server/src/Domain/Models/User.ts
+++ server/src/Domain/Models/User.ts
@@ -1,6 +1,7 @@
 import { pgTable, integer, varchar } from "drizzle-orm/pg-core";
 
-export const Seeders = pgTable("seeders", {
+export const User = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
+  password: varchar({ length: 255 }).notNull(),
 });
