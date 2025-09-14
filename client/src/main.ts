1ba1070e02e77496190c298a196cb9819d323fb9
roney ruiz <roneyruizrojas@gmail.com>
2025-09-13 21:43:43 -0600

    feat(client): setup prime vue theme and demo

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,14 @@
 import "./style.css";
+import { createApp } from "vue";
 import Main from "@App/Main.vue";
 
-import { createApp } from "vue";
+import { Theme } from "./theme";
 import { Router } from "./router";
+import PrimeVue from "primevue/config";
 
 const app = createApp(Main);
 
 app.use(Router);
+app.use(PrimeVue, Theme);
+
 app.mount("#app");
