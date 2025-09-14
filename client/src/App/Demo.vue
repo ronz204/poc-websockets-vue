4fc65f50710336a1a9b730288fa9bce35342ff9d
roney ruiz <roneyruizrojas@gmail.com>
2025-09-13 21:15:51 -0600

    feat(client): colors showcase

--- client/src/App/Demo.vue
+++ client/src/App/Demo.vue
@@ -3,8 +3,15 @@
 </script>
 
 <template>
-  <div class="demo">
-    <h1>Demo Component</h1>
-    <p>This is a demo component.</p>
+  <div class="demo bg-secondary-900 h-screen p-10">
+    <div class="flex gap-10">
+      <button class="bg-primary-500 py-2 px-4 rounded-xl">
+        <p class="text-white font-semibold">Demo Button</p>
+      </button>
+
+      <button class="bg-secondary-500 py-2 px-4 rounded-xl">
+        <p class="text-white font-semibold">Demo Button</p>
+      </button>
+    </div>
   </div>
 </template>
