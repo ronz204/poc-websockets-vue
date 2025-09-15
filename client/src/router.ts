db4172b4e6f70a610db3434b4b05385cba0eba34
roney ruiz <roneyruizrojas@gmail.com>
2025-09-14 21:21:45 -0600

    feat(client): create redirects in router

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
 
 import { DemoRoutes } from "@App/Demo/DemoRoutes";
 import { AuthRoutes } from "@App/Auth/AuthRoutes";
+
+const Redirects: RouteRecordRaw[] = [
+  { path: "/", redirect: "/signup" }
+];
 
 export const Router = createRouter({
   history: createWebHistory(), 
   routes: [
     ...DemoRoutes,
     ...AuthRoutes,
+    ...Redirects,
   ],
 });
