52ba3163eb9b82c712e54d3dcba0379ac3b24af1
roney ruiz <roneyruizrojas@gmail.com>
2025-09-16 22:04:52 -0600

    feat(client): create signup form component

--- client/src/App/Auth/Signup/SignupForm.vue
+++ client/src/App/Auth/Signup/SignupForm.vue
@@ -4,11 +4,36 @@ import Message from "primevue/message";
 
 import { InputText } from "primevue";
 import { Form } from "@primevue/forms";
-import { SignupSchema } from "./SignupSchema";
+import { SignupResolver } from "./SignupResolver";
+import type { FormSubmitEvent } from "@primevue/forms";
+
+const onSubmit = async (data: FormSubmitEvent) => {
+  console.log("submit form", data.values.name);
+};
 </script>
 
 <template>
-  <div>
-    <Message severity="error" size="small" variant="simple">Hello World</Message>
+  <div class="text-center space-y-3">
+    <p class="text-xl font-semibold">Create an Account</p>
+
+    <Form class="border-2 p-2" v-slot="$form"
+      :resolver="SignupResolver" @submit="onSubmit">
+
+      <div>
+        <InputText name="name" type="text" placeholder="Username" fluid />
+        <Message v-if="$form.name?.invalid" severity="error" variant="simple">
+          {{ $form.name.error.message }}
+        </Message>
+      </div>
+
+      <div>
+        <InputText name="password" type="password" placeholder="Password" fluid />
+        <Message v-if="$form.password?.invalid" severity="error" variant="simple">
+          {{ $form.password.error.message }}
+        </Message>
+      </div>
+
+      <Button type="submit" label="Register" fluid />
+    </Form>
   </div>
 </template>
--- client/src/App/Auth/Signup/SignupPage.vue
+++ client/src/App/Auth/Signup/SignupPage.vue
@@ -1,13 +1,11 @@
 <script lang="ts" setup>
-import SignupForm from "./SignupForm.vue";
+import SignupForm from './SignupForm.vue';
 </script>
 
 <template>
   <div class="min-h-screen flex items-center justify-center">
-    <div class="max-w-md w-full">
-
+    <div class="max-w-md w-full py-7 px-5 border-2">
       <SignupForm />
-      
     </div>
   </div>
 </template>
