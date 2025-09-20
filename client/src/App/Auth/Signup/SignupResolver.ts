dca6f737a615a4f4499010577d7933b9c3c14f8b
roney ruiz <roneyruizrojas@gmail.com>
2025-09-19 22:49:22 -0600

    chore(client): adjust error messages in sign up resolver

--- client/src/App/Auth/Signup/SignupResolver.ts
+++ client/src/App/Auth/Signup/SignupResolver.ts
@@ -3,7 +3,7 @@ import { yupResolver } from "@primevue/forms/resolvers/yup";
 
 export const SignupResolver = yupResolver(yup.object({
   name: yup.string().min(2).max(30)
-    .required("Name is required"),
+    .required("Please enter a username"),
   password: yup.string().min(6).max(30)
-    .required("Password is required"),
+    .required("Please enter a password"),
 }));
