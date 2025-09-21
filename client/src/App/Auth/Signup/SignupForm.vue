a1f16836235fa26d70ebbf2668b8c12cc49b0e00
roney ruiz <roneyruizrojas@gmail.com>
2025-09-20 21:11:52 -0600

    chore(client): move components from form to page

--- client/src/App/Auth/Signup/SignupForm.vue
+++ client/src/App/Auth/Signup/SignupForm.vue
@@ -15,33 +15,23 @@ const onSubmit = async (data: FormSubmitEvent) => {
 </script>
 
 <template>
-  <div class="text-center space-y-3">
-    <div class="text-center space-y-2">
-      <h1 class="text-2xl font-bold">Create an Account</h1>
-      <p>Join us to get started</p>
-    </div>
+  <Form class="space-y-4 p-2" :resolver="SignupResolver" @submit="onSubmit">
 
-    <Form class="space-y-4 p-2" :resolver="SignupResolver" @submit="onSubmit">
-      <FormField v-slot="$field" name="name">
-        <InputText type="text" placeholder="Username" fluid class="mb-1" />
-        <Message v-if="$field.invalid" severity="error" variant="simple">
-          {{ $field.error.message }}
-        </Message>
-      </FormField>
+    <FormField v-slot="$field" name="name">
+      <InputText type="text" placeholder="Username" fluid class="mb-1" />
+      <Message v-if="$field.invalid" severity="error" variant="simple">
+        {{ $field.error.message }}
+      </Message>
+    </FormField>
 
-      <FormField v-slot="$field" name="password">
-        <InputText type="password" placeholder="Password" fluid class="mb-1" />
-        <Message v-if="$field.invalid" severity="error" variant="simple">
-          {{ $field.error.message }}
-        </Message>
-      </FormField>
+    <FormField v-slot="$field" name="password">
+      <InputText type="password" placeholder="Password" fluid class="mb-1" />
+      <Message v-if="$field.invalid" severity="error" variant="simple">
+        {{ $field.error.message }}
+      </Message>
+    </FormField>
 
-      <Button type="submit" label="Register" fluid />
-    </Form>
+    <Button type="submit" label="Register" fluid />
 
-    <p class="text-sm font-semibold">
-      Already have an account?
-      <RouterLink to="/signin" class="underline">Login</RouterLink>
-    </p>
-  </div>
+  </Form>
 </template>
--- client/src/App/Auth/Signup/SignupPage.vue
+++ client/src/App/Auth/Signup/SignupPage.vue
@@ -1,11 +1,24 @@
 <script lang="ts" setup>
-import SignupForm from './SignupForm.vue';
+import SignupForm from "./SignupForm.vue";
 </script>
 
 <template>
   <div class="min-h-screen flex items-center justify-center background-pattern">
-    <div class="max-w-md w-full py-7 px-5 ring-2 rounded-xl bg-zinc-900">
+    <div class="max-w-md w-full py-7 px-5 ring-2 rounded-xl space-y-3 bg-zinc-900">
+
+      <div class="text-center space-y-2">
+        <h1 class="text-2xl font-bold">Create an Account</h1>
+        <p>Join us to get started</p>
+      </div>
+
       <SignupForm />
+
+      <div class="text-center">
+        <p class="text-sm font-semibold"> Already have an account?
+          <RouterLink to="/signin" class="underline">Login</RouterLink>
+        </p>
+      </div>
+      
     </div>
   </div>
 </template>
@@ -15,6 +28,9 @@ import SignupForm from './SignupForm.vue';
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("/src/Assets/Backgrounds/SimpleShiny.svg");
+
+  @media (min-width: 640px) {
+    background-image: url("/src/Assets/Backgrounds/SimpleShiny.svg");
+  }
 }
 </style>
